4cc05f609bd0627b4cb53394add3b521d4b45c2e
Sean Lennaerts <seanlenny@gmail.com>
2019-10-24 23:39:24 -0700

    Updates

---
+++ precache-manifest.96a9309b5d6e9c62a4cb914b32b7a4bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b7b9c4134d6f1b5051834191f9ff7c",
+    "url": "/wunderlist2standardnotes/index.html"
+  },
+  {
+    "revision": "a25f27b5e59559c9f30a",
+    "url": "/wunderlist2standardnotes/static/css/main.4c27656d.chunk.css"
+  },
+  {
+    "revision": "7cd95d78bc79d796d48a",
+    "url": "/wunderlist2standardnotes/static/js/2.d2817f5b.chunk.js"
+  },
+  {
+    "revision": "a25f27b5e59559c9f30a",
+    "url": "/wunderlist2standardnotes/static/js/main.ab8c7fa3.chunk.js"
+  },
+  {
+    "revision": "ae6b040d645917aa83e7",
+    "url": "/wunderlist2standardnotes/static/js/runtime-main.917c47aa.js"
+  }
+]);
